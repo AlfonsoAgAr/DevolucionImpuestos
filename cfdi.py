90a842ff33174b1b8e48588d19f1fc6a9dcd218e
Alfonso Aguilar <alfonso.aguilar@vultuscompany.com>
2022-06-13 12:49:32 -0500

    refactoring

--- cfdi.py
+++ cfdi.py
@@ -11,64 +11,49 @@ class CFDI3:
     """
     def __init__(self, filepath: str) -> None:
         self.xml = minidom.parse(filepath)
+
+        schemaLista = {
+            'General' : ['Total', 'SubTotal', 'Descuento', 'Fecha', 'Serie', 'Sello', 'NoCertificado', 'Certificado'],
+            'Emisor' : ['Nombre', 'RegimenFiscal', 'Rfc'],
+            'Concepto' : ['ClaveProdServ', 'ClaveUnidad', 'Descripción', 'Cantidad', 'Descuento', 'Importe', 'ValorUnitario'],
+            'Nomina' : ['FechaInicialPago', 'FechaFinalPago', 'FechaPago', 'NumDiasPagados', 'TotalDeducciones', 'TotalPercepciones'],
+            'Percepciones' : ['TotalExento', 'TotalGravado', 'TotalSueldos'],
+            'DesglosePercepciones' : ['Concepto', 'ImporteGravado', 'ImporteExento', 'TipoPercepcion'],
+            'Deducciones' : ['TotalImpuestosRetenidos', 'TotalOtrasDeducciones', 'TotalImpuestosTrasladados'],
+            }
     
         """ Datos generales del CFDI """
         self.General = self.xml.getElementsByTagName('cfdi:Comprobante')
-        self.total = self.General.item(0).attributes['Total'].value
-        self.subtotal = self.General.item(0).attributes['SubTotal'].value
-        self.descuento = self.General.item(0).attributes['Descuento'].value
-        self.fecha = self.General.item(0).attributes['Fecha'].value
-        self.series = self.General.item(0).attributes['Serie'].value
-        self.sello = self.General.item(0).attributes['Sello'].value
-        self.noCertificado = self.General.item(0).attributes['NoCertificado'].value
-        self.certificado = self.General.item(0).attributes['Certificado'].value
+        self.comprobante = {}
+        for elemento in schemaLista:
+            try:
+                self.comprobante.update(
+                    {
+                        f'{elemento}': self.General.item(0).attributes[f'{elemento}'].value
+                    })
+            except:
+                self.comprobante.update(
+                    {
+                        f'{elemento}': None
+                    })
 
         """ Datos del emisor del CFDI """
         self.Emisor = self.xml.getElementsByTagName('cfdi:Emisor')
-        self.nombreEmisor = self.Emisor.item(0).attributes['Nombre'].value
-        self.regimenFiscal = self.Emisor.item(0).attributes['RegimenFiscal'].value
-        self.rfcEmisor = self.Emisor.item(0).attributes['Rfc'].value
 
         """ Datos de los conceptos del CFDI """
         self.Concepto = self.xml.getElementsByTagName('cfdi:Concepto')
-        self.clave = self.Concepto.item(0).attributes['ClaveProdServ'].value
-        self.claveUnidad = self.Concepto.item(0).attributes['ClaveUnidad'].value
-        self.descripcion = self.Concepto.item(0).attributes['Descripcion'].value
-        self.cantidad = self.Concepto.item(0).attributes['Cantidad'].value
-        self.descuento = self.Concepto.item(0).attributes['Descuento'].value
-        self.importe = self.Concepto.item(0).attributes['Importe'].value
-        self.valorUnitario = self.Concepto.item(0).attributes['ValorUnitario'].value
 
         """ Datos de la nómina """
         self.Nomina = self.xml.getElementsByTagName('nomina12:Nomina')
-        self.fechaInicialPago = self.Nomina.item(0).attributes['FechaInicialPago'].value
-        self.fechaFinalPago = self.Nomina.item(0).attributes['FechaFinalPago'].value
-        self.fechaPago = self.Nomina.item(0).attributes['FechaPago'].value
-        self.diasPagados = self.Nomina.item(0).attributes['NumDiasPagados'].value
-        self.totalDeduccion = self.Nomina.item(0).attributes['TotalDeducciones'].value
-        self.totalPercibido = self.Nomina.item(0).attributes['TotalPercepciones'].value
 
         """ Total de percepciones del asalariado """
         self.Percepciones = self.xml.getElementsByTagName('nomina12:Percepciones')
-        self.totalExento = self.Percepciones.item(0).attributes['TotalExento'].value
-        self.totalGravado = self.Percepciones.item(0).attributes['TotalGravado'].value
-        self.totalSueldo = self.Percepciones.item(0).attributes['TotalSueldos'].value
 
         """ Listado de las percepciones """
         self.DesglosePercepciones = self.xml.getElementsByTagName('nomina12:Percepcion')
-        self.listaPercepciones = [
-            {
-                'Concepto': itemPercepcion.attributes['Concepto'].value,
-                'ImporteGravado': itemPercepcion.attributes['ImporteGravado'].value,
-                'ImporteExento': itemPercepcion.attributes['ImporteExento'].value,
-                'TipoPercepcion': itemPercepcion.attributes['TipoPercepcion'].value
-            }
-            for itemPercepcion in self.DesglosePercepciones]
 
         """ Total de Deducciones del asalariado """
         self.Deducciones = self.xml.getElementsByTagName('nomina12:Deducciones')
-        self.impuestosRetenidos = self.Deducciones.item(0).attributes['TotalImpuestosRetenidos'].value
-        self.otrasDeducciones = self.Deducciones.item(0).attributes['TotalOtrasDeducciones'].value
 
         """ Listado de las deducciones """
         self.DesgloseDeducciones = self.xml.getElementsByTagName('nomina12:Deduccion')
